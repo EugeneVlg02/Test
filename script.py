b12c9e00661441bf7e2ec6c2a67be93de1023622
EugeneVlg02 <astra59302@gmail.com>
2022-09-23 08:31:25 +0300

    V2.0

--- script.py
+++ script.py
@@ -1,4 +1,12 @@
+# Recursion #
 def fibo(n):
     return 1 if n <=2 else fibo(n-1) + fibo(n-2)
-for i in range(1, 10):
-    print(fibo(i))
+
+# Dynamic programming #
+def fibo2(n):
+    result = [0 for i in range(n+1)]
+    result[0] = 1
+    result[1] = 1
+    for i in range(2, n+1):
+        result[i] = result[i-2] + result[i-1]
+    return result[n-1]
